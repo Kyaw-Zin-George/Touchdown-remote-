feb65dfe18879b6328244078ce8fe2da8af75efe
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-15 23:43:00 +0100

    Adding animation to the logo view in the nav view

--- Touchdown/View/Home/NavigationBarView.swift
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct NavigationBarView: View {
     //MARK: - Properties
-    
+    @State private var isAnimated: Bool = false
     //MARK: - Body
     var body: some View {
         HStack{
@@ -19,11 +19,26 @@ struct NavigationBarView: View {
                     .foregroundStyle(.black)
             })
             Spacer()
-            Button(action:{}, label: {
-                Image(systemName: "cart")
-                    .font(.title)
-                    .foregroundStyle(.black)
-            })
+            LogoView()
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x:0, y: isAnimated ? 0 : -25)
+                .onAppear {
+                    withAnimation(.easeOut(duration: 0.5)){
+                        isAnimated.toggle()
+                    }
+                }
+            Spacer()
+            ZStack {
+                Button(action:{}, label: {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundStyle(.black)
+                })
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 13, height: 13)
+                    .offset(x:13,y: -10)
+            }//ZStack
         }
     }
 }
