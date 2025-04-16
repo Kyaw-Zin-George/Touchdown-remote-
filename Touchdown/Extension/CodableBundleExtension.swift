6bc8b4a1135b0b1917a9d4ec4229b9ba4bfbd996
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-16 23:49:24 +0100

    Creating extension of bundle for decoding json files faster

--- Touchdown/Extension/CodableBundleExtension.swift
+++ Touchdown/Extension/CodableBundleExtension.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+extension Bundle{
+    func decode<T: Codable>( _ file: String) -> T{
+        // Locate json file
+        guard let url = self.url(forResource: file, withExtension: nil) else{
+            fatalError("Couldn't find \(file) in bundle.")
+        }
+        //create property for the data
+        guard let data = try? Data(contentsOf: url) else{
+            fatalError("Couldn't load \(file) as Data.")
+        }
+        //create decoder
+        let decoder = JSONDecoder()
+        
+        //create property for the decoder
+        guard let decodedData = try? decoder.decode(T.self, from: data) else{
+            fatalError("Couldn't decode \(file) as \(T.self).")
+        }
+        //return ready to use data
+        return decodedData
+    }
+}
