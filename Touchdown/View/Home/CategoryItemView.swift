dffe0af546e7743d2e1ef4ae4afe30917d53b2b7
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-17 23:39:08 +0100

    Creating category item view

--- Touchdown/View/Home/CategoryItemView.swift
+++ Touchdown/View/Home/CategoryItemView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct CategoryItemView: View {
+    //MARK: - Properties
+    let category: Category
+    //MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {}, label: {
+            HStack(alignment: .center,spacing: 6){
+                Image(category.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30,alignment: .center)
+                    .foregroundStyle(.gray)
+                Text(category.name.uppercased())
+                    .font(.title2)
+                    .fontWeight(.light)
+                    .foregroundStyle(.gray)
+                Spacer()
+            }//hstack
+            .padding()
+            .background(Color.white.clipShape(RoundedRectangle(cornerRadius: 12)))
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+        })//button
     }
 }
 
 #Preview {
-    CategoryItemView()
+    CategoryItemView(category: categories[0])
+        .padding()
 }
