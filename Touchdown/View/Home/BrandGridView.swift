302dd9f0f9b186d3c6b7b20827ea3fafb1b4f1b7
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-19 22:49:27 +0100

    Creating brand grid view

--- Touchdown/View/Home/BrandGridView.swift
+++ Touchdown/View/Home/BrandGridView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct BrandGridView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: gridLayout,spacing: rowSpacing) {
+                ForEach(brands){ brand in
+                    BrandItemView(brand: brand)
+                    
+                }
+            }.frame(height: 200)
+                .padding(15)
+        }
     }
 }
 
-#Preview {
+#Preview(traits: .sizeThatFitsLayout) {
     BrandGridView()
+        .background(colorBackground)
 }
