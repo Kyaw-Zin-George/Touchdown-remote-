2e11724e57ec43f60b89fb89ae7c92981bca5cde
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-21 00:18:51 +0100

    Developing quantity and favorite ui buttons

---
+++ Touchdown/Components/Details/AddToCartDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AddToCartDetailView.swift
+//  Touchdown
+//
+//  Created by Kyaw Thant Zin(George) on 4/21/25.
+//
+
+import SwiftUI
+
+struct AddToCartDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddToCartDetailView()
+}
---
+++ Touchdown/Components/Details/QuantityFavoriteDetailView.swift
@@ -0,0 +1,47 @@
+//
+//  QuantityFavoriteDetailView.swift
+//  Touchdown
+//
+//  Created by Kyaw Thant Zin(George) on 4/20/25.
+//
+
+import SwiftUI
+
+struct QuantityFavoriteDetailView: View {
+    @State private var counter: Int = 0
+    var body: some View {
+        HStack(alignment: .center,spacing: 3) {
+            Button(action: {
+                if counter > 0{
+                    counter -= 1
+                }
+            }) {
+                Image(systemName: "minus.circle")
+                    
+            }
+            Text("\(counter)")
+                .fontWeight(.bold)
+                .frame(minWidth: 36)
+            Button(action: {
+                if counter < 100{
+                    counter += 1
+                }
+            }) {
+                Image(systemName: "plus.circle")
+            }
+            Spacer()
+            
+            Button(action: {}) {
+                Image(systemName: "heart")
+                    .foregroundStyle(.pink)
+            }
+        }.font(.system(.title, design: .rounded))
+            .foregroundStyle(.black)
+            .imageScale(.large)
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    QuantityFavoriteDetailView()
+        .padding()
+}
