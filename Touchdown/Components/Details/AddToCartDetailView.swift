e5836d7f68bc3e3856c3c8ddf8259c600cb0f534
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-21 00:19:20 +0100

    Creating add to cart button view

--- Touchdown/Components/Details/AddToCartDetailView.swift
+++ Touchdown/Components/Details/AddToCartDetailView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct AddToCartDetailView: View {
+    @State private var isPressed: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action:{}){
+            Spacer()
+            Text("Add to cart".uppercased())
+                .font(.system(.title2,design: .rounded))
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+            Spacer()
+        }
+        .padding(15)
+        .background(Color(red: sampleProduct.red, green: sampleProduct.green, blue: sampleProduct.blue))
+        .clipShape(Capsule())
+        .contentShape(Capsule())
+        .scaleEffect(isPressed ? 0.95 : 1)
+        .animation(.easeOut(duration: 0.75),value: isPressed)
     }
 }
 
-#Preview {
+#Preview (traits: .sizeThatFitsLayout){
     AddToCartDetailView()
+        .padding()
 }
