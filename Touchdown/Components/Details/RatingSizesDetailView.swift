fcd803387294a5f30e6f1918935540c26a6880da
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-20 23:57:28 +0100

    Creating rating and sizes design

--- Touchdown/Components/Details/RatingSizesDetailView.swift
+++ Touchdown/Components/Details/RatingSizesDetailView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct RatingSizesDetailView: View {
+    
+    let sizes: [String] = ["XS", "S", "M", "L", "XL"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .top) {
+            //ratings
+            VStack(alignment: .leading,spacing: 3) {
+                Text("Ratings")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(colorGray)
+                
+                HStack(alignment: .center,spacing: 3) {
+                    ForEach(1...5, id: \.self){ item in
+                    Button(action: {}) {
+                            Image(systemName: "star.fill")
+                                .frame(width: 28, height: 28)
+                                .background(colorGray.clipShape(RoundedRectangle(cornerRadius: 5)))
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+            }
+            Spacer()
+            //sizes
+            VStack(alignment: .trailing,spacing: 3) {
+                Text("Sizes")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(colorGray)
+                
+                HStack(alignment: .center,spacing: 3) {
+                    ForEach(sizes, id: \.self){ size in
+                        Button(action: {}) {
+                            Text(size)
+                                .font(.footnote)
+                                .foregroundStyle(colorGray)
+                                .frame(width: 28, height: 28)
+                                .background(Color.white.clipShape(RoundedRectangle(cornerRadius: 5)))
+                                .background(RoundedRectangle(cornerRadius: 5).stroke(colorGray,lineWidth: 1))
+                        }
+                    }
+                }
+            }
+            
+        }
     }
 }
 
-#Preview {
+#Preview(traits: .sizeThatFitsLayout) {
     RatingSizesDetailView()
+        .padding()
 }
