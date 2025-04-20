d2d7ffd71f44fbc169b1df5a8cb9a11446f24ee5
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-20 19:53:25 +0100

    Implementing 3 sub views ( navbar, header and top part of detail) into the detail view

--- Touchdown/App/ProductDetailView.swift
+++ Touchdown/App/ProductDetailView.swift
@@ -9,10 +9,35 @@ import SwiftUI
 
 struct ProductDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading,spacing: 5) {
+            //NavBar
+            NavBarDetailView()
+                .padding(.horizontal)
+                .padding(.top)
+            //Header
+            HeaderDetailView()
+                .padding(.horizontal)
+            //Detail Top part
+            TopPartDetailView()
+                .padding(.horizontal)
+            //Detail bottom bart
+            // ratings sizes
+            //description
+            //quantity favorite
+            //add to cart
+            Spacer()
+        }//Vstack
+        .ignoresSafeArea()
+        .background(
+            Color(
+                red: sampleProduct.red,
+                green: sampleProduct.green,
+                blue: sampleProduct.blue)
+        ).ignoresSafeArea(.all,edges: .all)
     }
 }
 
-#Preview {
+#Preview(traits: .fixedLayout(width: 375, height: 812)) {
     ProductDetailView()
 }
